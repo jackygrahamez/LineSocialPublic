ff7648215b6fcd1833f8ae924ea2c5a462afe6c7
Jack Shultz <jshultz@thelathe.com>
2013-11-17 15:29:04 -0500

    instead of using localhost use the current location parameter for socket listener

--- public/js/main.js
+++ public/js/main.js
@@ -2,54 +2,10 @@
   var x=document.getElementById("demo");
   var lineLength=0;
   var global_cID="";
+  var global_fID="";
+  var global_tID="";
 (function($, window, undefined) {
- window.Chat = {
-    socket : null,
 
-    initialize : function(socketURL) {
-      this.socket = io.connect(socketURL);
-
-      //Send message on button click or enter
-      $('#send').click(function() {
-        Chat.send();
-      });
-
-      $('#message').keyup(function(evt) {
-        if ((evt.keyCode || evt.which) == 13) {
-          Chat.send();
-          return false;
-        }
-      });
-
-      //Process any incoming messages
-      this.socket.on('new', this.add);
-    },
-
-    //Adds a new message to the chat.
-    add : function(data) {
-      var name = data.name || 'anonymous';
-      var msg = $('<div class="msg"></div>')
-        .append('<span class="name">' + name + '</span>: ')
-        .append('<span class="text">' + data.msg + '</span>');
-
-      $('#messages')
-        .append(msg)
-        .animate({scrollTop: $('#messages').prop('scrollHeight')}, 0);
-    },
-
-    //Sends a message to the server,
-    //then clears it from the textarea
-    send : function() {
-      this.socket.emit('msg', {
-        name: $('#name').val(),
-        msg: $('#message').val()
-      });
-      
-      $('#message').val('');
-    }
-  };
-
-  Chat.initialize('http://localhost/');
 
       $('.on_off :checkbox').iphoneStyle();
       $('.disabled :checkbox').iphoneStyle();
