50e5846ddf528e4702a373f95697348e616bf9f0
Jack Shultz <jshultz@thelathe.com>
2013-11-29 18:08:03 -0500

    updated the messages sending

--- models/message.js
+++ models/message.js
@@ -3,13 +3,15 @@ module.exports = function(mongoose) {
   ObjectId = Schema.ObjectId;
 	
   var messageSchema = new mongoose.Schema({
-	  	ts: {type: String },
+	  	ts: {type: Number },
 	    cID: { type: ObjectId },
 	    fID: { type: ObjectId },
 	    tID: { type: ObjectId },
 	    message: { type: String},
 	    requests: { type: String},
-	    user: { type: String}
+	    user: { type: String},
+	    time: { type: String},
+	    messageDate: { type: Date }
   	});
   
   var message = mongoose.model('Message', messageSchema);  
@@ -27,7 +29,18 @@ module.exports = function(mongoose) {
   };
 
   var saveMessages = function(ts, cID, fID, tID, user_messages, user, callback) {
-	  query = { ts: ts, cID: cID, fID: fID, tID: tID, message: user_messages, user: user }
+	  var d = new Date();
+	  var h = d.getHours();
+	  var td = "am";
+	  if (h > 12) { 
+	  h = h - 12;
+	  td = "pm"; 
+	  }
+	  var m = d.getMinutes();
+	  if (m < 10) { m = "0"+m };   
+	  var time = " " + h + ":" + m + " "+td;
+	  var timestamp = d.getTime(); 
+	  query = { ts: ts, cID: cID, fID: fID, tID: tID, message: user_messages, user: user, time: time, messageDate: d }
 	  var im = new message(query); 
 	  console.log("saveMEssages query "+JSON.stringify(query));
 	  im.save(callback)
