a3ddc5e51f79497c6bcebd07c38c5be6df90193a
Jack Shultz <jshultz@thelathe.com>
2013-11-30 23:02:28 -0500

    implemented Facebook Authentication

--- app.js
+++ app.js
@@ -7,11 +7,82 @@ var express = require('express'),
     routes = require('./routes'),
     user = require('./routes/user'),
     http = require('http'),
-    path = require('path');
+    path = require('path'),
+    passport = require('passport'),
+    util = require('util'),
+    FacebookStrategy = require('passport-facebook').Strategy;
 
 var app    = express();
 var server = http.createServer(app);
 global.io  = require('socket.io').listen(server);
+
+/* FACEBOOK AUTHENTICATION */
+var FACEBOOK_APP_ID = "698217933545116"
+var FACEBOOK_APP_SECRET = "7f8e2e6662a925a67b26d56063f3577e";
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Facebook profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the FacebookStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Facebook
+//   profile), and invoke a callback with a user object.
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Facebook profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Facebook account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+// GET /auth/facebook
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Facebook authentication will involve
+//   redirecting the user to facebook.com.  After authorization, Facebook will
+//   redirect the user back to this application at /auth/facebook/callback
+app.get('/auth/facebook',
+  passport.authenticate('facebook'),
+  function(req, res){
+    // The request will be redirected to Facebook for authentication, so this
+    // function will not be called.
+  });
+
+// GET /auth/facebook/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+/* END FACEBOOK AUTHENTICATION */
 
 
 console.log("using authenticator");
@@ -104,16 +175,3 @@ server.listen(app.get('port'), function(){
 
 });
 
-
-/*
-var port = 5000;
-var io = require('socket.io').listen(app.listen(port));
-
-global.io.sockets.on('connection', function (socket) {
-	socket.emit('message', { message: 'welcome to the chat' });
-	socket.on('send', function (data) {
-		global.io.sockets.emit('message', data);
-	});
-});
-console.log("Listening on port " + port);  
-*/
