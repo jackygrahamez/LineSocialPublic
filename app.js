098d96afd88f0dc79dc80615a45dc05770ec319b
Jack Shultz <jshultz@thelathe.com>
2014-01-08 19:58:19 -0500

    updated the password reset

--- app.js
+++ app.js
@@ -109,8 +109,62 @@ passport.use(new FacebookStrategy({
 
 /* END FACEBOOK AUTHENTICATION */
 /* FORGOT PASSWORD */
+app.use(express.static(__dirname));
+app.use(require('sesame')()); // for sessions
 
+// example nodemailer config here
+var forgot = require('password-reset-nodemailer')({
+  uri: 'http://localhost:5000/password_reset',
+  from: 'webmater@linesocial.mobi',
+  transportType: 'SMTP',
+  transportOptions: {
+    service: "Gmail",
+    auth: {
+        user: "webmaster@linesocial.mobi",
+        pass: "Tgifkfc123"
+    }
+  }
+});
 
+app.use(forgot.middleware);
+
+app.post('/forgot', express.bodyParser(), function(req, res) {
+	  var email = req.body.email;
+
+	  var callback = {
+	    error: function(err) {
+	      res.end('Error sending message: ' + err);
+	    },
+	    success: function(success) {
+	      res.end('Check your inbox for a password reset message.');
+	    }
+	  };
+	  var reset = forgot(email, callback);
+
+	  reset.on('request', function(req_, res_) {
+	    req_.session.reset = {
+	      email: email,
+	      id: reset.id
+	    };
+	    fs.createReadStream(__dirname + '/forgot.html').pipe(res_);
+	  });
+	});
+
+app.post('/reset', express.bodyParser(), function(req, res) {
+  if (!req.session.reset) return res.end('reset token not set');
+
+  var password = req.body.password;
+  var confirm = req.body.confirm;
+  if (password !== confirm) return res.end('passwords do not match');
+
+  // update the user db here
+
+  forgot.expire(req.session.reset.id);
+  delete req.session.reset;
+  res.end('password reset');
+});
+  
+	  
 /* END FORGOT PASSWORD */
 
 // development only
