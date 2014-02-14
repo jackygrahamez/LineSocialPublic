0b3c7424c4ff0f30ba34348bdfea1502534ca9c2
Jack Shultz <jshultz@thelathe.com>
2014-02-14 00:17:42 -0500

    user register

--- example/linesocial_spec.js
+++ example/linesocial_spec.js
@@ -1,3 +1,14 @@
+var timeout = 10000,
+    number = Math.floor((Math.random()*10000000000)+1),
+    firstname = "qa_first",
+    lastname = "qa_last",
+    username = "qa_"+number,
+    email = username+"@test.com",
+    password = "abc";    
+
+browser.driver.manage().timeouts().setScriptTimeout = 2000;
+
+console.log("number "+username);
 
 describe('LineSocial homepage', function() {
   it('Test Title', function() {
@@ -23,12 +34,9 @@ describe('LineSocial homepage', function() {
     	});   	
   });
 
-
-  describe('LineSocial Register Page', function() {
-
-	  it('Register Page', function() {
-	    browser.driver.get('http://localhost:5000')
-	    	.then(function(){  
+  it('Register Page', function() {
+      browser.driver.get('http://localhost:5000')
+        .then(function(){
           browser.driver.executeScript("$(\"#register\").click()");
           setTimeout(function(){
             var promise = browser.driver.findElement(by.css('#submit')).getAttribute('value');
@@ -36,13 +44,9 @@ describe('LineSocial homepage', function() {
               console.log("submit is: " + buttonText);
               expect(buttonText).toMatch('Register Now');
             });
-          }, 5000);            
-	    	});   	
-	  });
-
+          }, timeout);            
+        });     
   });
-
-  describe('LineSocial Register User', function() {
 
     it('Register User', function() {
       browser.driver.get('http://localhost:5000')
@@ -51,13 +55,29 @@ describe('LineSocial homepage', function() {
           setTimeout(function(){
             var promise = browser.driver.findElement(by.css('#submit')).getAttribute('value');
             promise.then(function(buttonText) {
+              browser.driver.findElement(By.css("input[name='firstName']")).sendKeys(firstname);
+              browser.driver.findElement(By.css("input[name='lastName']")).sendKeys(lastname);
+              browser.driver.findElement(By.css("input[name='username']")).sendKeys(username);
+              browser.driver.findElement(By.css("input[name='email']")).sendKeys(email);
+              browser.driver.findElement(By.css("input[name='cemail']")).sendKeys(email);
+              browser.driver.findElement(By.css("input[name='password']")).sendKeys(password);
+              browser.driver.findElement(By.css("input[name='cpassword']")).sendKeys(password); 
+              browser.driver.executeScript("$('#terms').prop('checked', true);");
+              browser.driver.executeScript("$(\"#submit\").click()");                                             
               console.log("submit is: " + buttonText);
-              expect(buttonText).toMatch('Register Now');
-            });
-          }, 5000);            
-        });     
-    });
+              setTimeout(function(){
+                var promise = browser.driver.findElement(by.css("a[href='/forgot_password']")).getText();
 
-  });
+                promise.then(function(forgot_password) {
+                  console.log("User Registered. Homepage forgot password: " + forgot_password);
+                  expect(forgot_password).toMatch('Forgot Password?');
+                });                
+              }, timeout);
+            });
+          }, timeout);            
+        });     
+    });  
 
 });
+
+ 
